dabb6f51eb58e65f77a34147634546bad0bd1fcb
xxlxep <xxlxep@gmail.com>
2019-05-22 12:36:51 +0300

    Data transformation in swapiService & random planet component finished

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,12 +1,55 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import './random-planet.css';
+import SwapiService from '../../services/swapi-service';
 
-export default class randomPlanet extends Component {
+export default class RandomPlanet extends Component {
+  swapiService = new SwapiService();
+
+  state = {
+    planet: {},
+  };
+
+  constructor() {
+    super();
+    this.updatePlanet();
+  }
+
+  onPlanetLoaded = planet => {
+    this.setState({ planet });
+  };
+  updatePlanet() {
+    const id = Math.floor(Math.random() * 25 + 2);
+    this.swapiService.getPlanet(id).then(this.onPlanetLoaded);
+  }
   render() {
+    const {
+      planet: { id, name, population, rotationPeriod, diameter },
+    } = this.state;
     return (
       <div className="random-planet jumbotron round">
-        <img className="planet-image" src="https://starwars-visualguide.com/assets/img/planets/9.jpg" alt="planet"></img>
+        <img
+          className="planet-image"
+          src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
+          alt="planet"
+        />
+        <div>
+          <h4>{name}</h4>
+          <ul className="list-group list-group-fush">
+            <li className="list-group-item">
+              <span className="term">Population </span>
+              <span>{population}</span>
+            </li>
+            <li className="list-group-item">
+              <span className="term">Rotation Period </span>
+              <span>{rotationPeriod}</span>
+            </li>
+            <li className="list-group-item">
+              <span className="term">Diameter </span>
+              <span>{diameter}</span>
+            </li>
+          </ul>
+        </div>
       </div>
-    )
+    );
   }
 }
