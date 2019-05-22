3042f15392710662596a7ff43faaad8ca8c9d769
xxlxep <xxlxep@gmail.com>
2019-05-22 17:21:47 +0300

    spinner & Error handling added

---
+++ src/components/error-indicator/error-indicator.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ErrorIndicator = () => {
+  return (
+    <div className="error-indicator">
+      <span className="boom">BOOM! </span>
+      <span>Something has gone terribly wrong</span>
+    </div>
+  );
+};
+
+export default ErrorIndicator;
