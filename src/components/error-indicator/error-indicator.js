2db9085a2119b6a7bf3f370ec83df0b47cfc3a99
xxlxep <xxlxep@gmail.com>
2019-05-24 12:06:25 +0300

    design fix

--- src/components/error-indicator/error-indicator.js
+++ src/components/error-indicator/error-indicator.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import icon from './death-star.png';
+import './error-indicator.css';
 
 const ErrorIndicator = () => {
   return (
     <div className="error-indicator">
+      <img src={icon} alt="error icon" />
       <span className="boom">BOOM! </span>
       <span>Something has gone terribly wrong</span>
     </div>
